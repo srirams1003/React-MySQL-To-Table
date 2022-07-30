cf3df4aa48072c0f1106011cb0bc3bab29f29e08
Sriram Suresh <sriram.suresh@terralogic.com>
2022-07-30 12:12:24 +0530

    fixed pretty print

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import React, { useEffect, useState, useRef } from 'react';
 import './App.css';
+import ReactJson from 'react-json-view';
 
 function App() {
 
   const [data, setData] = useState(null);
 
   async function apiCall(){
-    let response = await fetch('/api/getAllTypes');
+    let response = await fetch('/api/asset/2');
     response = await response.json();
     console.log(response);
-    setData(response[0].typeName);
+    // setData(response[0].typeName);
+    setData(response);
   }
 
   useEffect(function () {
@@ -22,10 +24,13 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+      </header> */}
+      
+      <div>
+        <div>{!data ? "Loading..." : <ReactJson src={data} theme="monokai" /> }</div>
+      </div>
     </div>
   );
 
