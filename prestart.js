61593fc40193aaa4f5d9d1b178552df1cd9a93af
Sriram Suresh <sriram.suresh449@gmail.com>
2025-07-05 09:03:38 -0700

    added host and port flags to mysql exec command in prestart.js

--- prestart.js
+++ prestart.js
@@ -1,6 +1,5 @@
 const mysql = require('mysql2/promise');
-const {exec} = require('child_process');
-
+const { exec } = require('child_process');
 require('dotenv').config();
 
 const mysqlConnectionObject = {
@@ -13,16 +12,26 @@ const mysqlConnectionObject = {
 mysql.createConnection(mysqlConnectionObject)
     .then((connection) => {
         connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.SQL_DB_NAME}\`;`)
-            .then((result) => {
-                // Import the database.
-                let dumpFile = './dump.sql';
-                exec(`mysql -u${mysqlConnectionObject.user} -p${mysqlConnectionObject.password} ${process.env.SQL_DB_NAME} < ${dumpFile}`, (err, stdout, stderr) => {
-                    if (err) {console.error(`exec error: ${err}`); return;}
+            .then(() => {
+                const dumpFile = './dump.sql';
 
+                // Build the CLI command with host/port specified
+                const importCommand = `mysql -h${mysqlConnectionObject.host} -P${mysqlConnectionObject.port} -u${mysqlConnectionObject.user} -p${mysqlConnectionObject.password} ${process.env.SQL_DB_NAME} < ${dumpFile}`;
+
+                exec(importCommand, (err, stdout, stderr) => {
+                    if (err) {
+                        console.error(`exec error: ${err}`);
+                        return;
+                    }
+                    if (stderr) {
+                        console.error(`stderr: ${stderr}`);
+                    }
                     console.log(`The import has finished.`);
                 });
 
                 connection.end();
             });
+    })
+    .catch((err) => {
+        console.error(`MySQL connection failed: ${err.message}`);
     });
-
